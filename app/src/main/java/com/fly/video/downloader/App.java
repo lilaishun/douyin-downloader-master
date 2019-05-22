f524f37cc4432ce5af99f39ef479778b1e71c5c4
lilaishun <lls_android@163.com>
2019-05-22 15:35:44 +0800

    添加支付功能

--- app/src/main/java/com/fly/video/downloader/App.java
+++ app/src/main/java/com/fly/video/downloader/App.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.fly.iconify.Iconify;
 import com.fly.iconify.fontawesome.module.FontAwesomeLightModule;
 import com.fly.iconify.fontawesome.module.FontAwesomeModule;
+import com.payelves.sdk.EPay;
 
 public class App extends Application {
 
@@ -18,6 +19,21 @@ public class App extends Application {
                 .with(new FontAwesomeModule());
 
         app = this;
+        /**
+         * 支付服务初始化
+         * @param openId
+         *      用户id(不能为null和空字符串,区分大小写,数据来源:后台->设置->API接口信息->OPEN_ID)
+         * @param token
+         *      秘钥(不能为null和空字符串,区分大小写,数据来源:后台->设置->API接口信息->TOKEN)
+         * @param appKey
+         *      appKey(不能为null和空字符串,数据来源:后台->应用->该应用appKey)
+         * @param channel
+         *      channel(不能为null和空字符串)"baidu","xiaomi" ,"360"
+         * @return
+         */
+        EPay.getInstance(getApplicationContext()).init("C8KjKKIA9","0499a1d129ef48958871426985154aab",
+               " \t8328148005879813", "baidu");
+
     }
 
     public static Application getApp()
--- app/src/main/java/com/fly/video/downloader/MainActivity.java
+++ app/src/main/java/com/fly/video/downloader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fly.video.downloader;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -19,6 +21,9 @@ import com.fly.video.downloader.util.content.Recv;
 import com.fly.video.downloader.util.model.Video;
 import com.github.florent37.runtimepermission.RuntimePermission;
 import com.github.florent37.runtimepermission.callbacks.PermissionListener;
+import com.payelves.sdk.EPay;
+import com.payelves.sdk.enums.EPayResult;
+import com.payelves.sdk.listener.PayResultListener;
 
 import java.util.Date;
 import java.util.List;
@@ -102,6 +107,12 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onDenied(RuntimePermission runtimePermission, List<String> denied, List<String> foreverDenied) {
                 Toast.makeText(MainActivity.this,"Why?", Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.pay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pay();
             }
         });
 
@@ -206,4 +217,50 @@ public class MainActivity extends BaseActivity {
         historyFragment.perpendHistory(video);
     }
 
+
+
+
+
+    private void  pay(){
+        /**
+         * 发起支付
+         *
+         * @param subject       商品名称,不可为空和空字符串
+         * @param body          商品内容,不可为空和空字符串
+         * @param amount        支付金额，单位分，不能为null和<1
+         * @param orderId       商户系统的订单号(如果有订单的概念),没有可为空
+         * @param payUserId     商户系统的用户id(如果有用户的概念),没有可为空
+         * @param backPara      支付成功后支付精灵会用此参数回调配置的url
+         *					(回调url在后台应用->添加应用时候配置)
+         * demo: backPara 的value(建议json) ： {"a":1,"b":"2"},如不需要可为空。
+         * @param payResultListener，不能为null 支付结果回调
+         */
+        EPay.getInstance(this).pay("视频下载", "视频下载量", 1,
+                "", "","", new PayResultListener() {
+                    /**
+                     * @param context
+                     * @param payId   支付精灵支付id
+                     * @param orderId   商户系统订单id
+                     * @param payUserId 商户系统用户ID
+                     * @param payResult
+                     * @param payType   支付类型:1 支付宝，2 微信 3 银联
+                     * @param amount    支付金额
+                     * @see EPayResult#FAIL_CODE
+                     * @see EPayResult#SUCCESS_CODE
+                     * 1支付成功，2支付失败
+                     */
+                    @Override
+                    public void onFinish(Context context, Long payId, String orderId, String payUserId,
+                                         EPayResult payResult , int payType, Integer amount) {
+                        EPay.getInstance(context).closePayView();//关闭快捷支付页面
+                        if(payResult.getCode() == EPayResult.SUCCESS_CODE.getCode()){
+                            //支付成功逻辑处理
+                            Toast.makeText(MainActivity.this, payResult.getMsg(), Toast.LENGTH_LONG).show();
+                        }else if(payResult.getCode() == EPayResult.FAIL_CODE.getCode()){
+                            //支付失败逻辑处理
+                            Toast.makeText(MainActivity.this, payResult.getMsg(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 }
